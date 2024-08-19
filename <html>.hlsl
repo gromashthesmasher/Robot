791d57a598230cea33d1b9bf9b5a2ecaaf298841
gromashthesmasher <134214734+gromashthesmasher@users.noreply.github.com>
2024-08-19 05:14:28 -0400

    Create <html>.hlsl

---
+++ <html>.hlsl
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title>Counter App</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Counter App</h1>
+        <div class="counter">
+            <button id="increment-btn">+</button>
+            <div id="counter-value">0</div>
+            <button id="decrement-btn">-</button>
+        </div>
+        <button id="reset">Reset</button>
+    </div>
+
+    <script src="counter.js"></script>
+</body>
+
+</html>
